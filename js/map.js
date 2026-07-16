5aead66cdbd8775cd2739e49d5ca554d4b00e924
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-16 16:48:56 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,37 @@
+// Khởi tạo bản đồ
 
+const map = L.map("map").setView([21.386,103.023],9);
+
+// Bản đồ nền
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    attribution:'© OpenStreetMap'
+}).addTo(map);
+
+// Đọc GeoJSON
+
+fetch("data/dienbien_xa.geojson")
+
+.then(r=>r.json())
+
+.then(data=>{
+
+    L.geoJSON(data,{
+
+        style:{
+
+            color:"#1976D2",
+
+            weight:1.5,
+
+            fillColor:"#64B5F6",
+
+            fillOpacity:0.35
+
+        }
+
+    }).addTo(map);
+
+});
