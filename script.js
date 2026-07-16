a7d873618eefc1fb7fae972a12dd0d437fa4f973
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-16 16:00:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Đọc file GeoJSON
-fetch("dienbien_xa.geojson")
+fetch("data/dienbien_xa.geojson")
     .then(response => response.json())
     .then(data => {
 
