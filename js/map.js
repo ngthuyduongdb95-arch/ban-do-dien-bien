b2b4851b856b248f2c1482ed8972fd825b64bf1a
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-17 07:49:36 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,37 +1,36 @@
 // Khởi tạo bản đồ
-
-const map = L.map("map").setView([21.386,103.023],9);
+const map = L.map("map").setView([21.386, 103.023], 9);
 
 // Bản đồ nền
-
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-    attribution:'© OpenStreetMap'
-}).addTo(map);
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "© OpenStreetMap"
+    }
+).addTo(map);
 
 // Đọc GeoJSON
-
 fetch("data/dienbien_xa.geojson")
+    .then(response => {
+        console.log("Status:", response.status);
+        console.log("Content-Type:", response.headers.get("content-type"));
+        return response.text();
+    })
+    .then(text => {
+        console.log("Nội dung đầu file:");
+        console.log(text.substring(0, 200));
 
-.then(r=>r.json())
+        const data = JSON.parse(text);
 
-.then(data=>{
-
-    L.geoJSON(data,{
-
-        style:{
-
-            color:"#1976D2",
-
-            weight:1.5,
-
-            fillColor:"#64B5F6",
-
-            fillOpacity:0.35
-
-        }
-
-    }).addTo(map);
-
-});
+        L.geoJSON(data, {
+            style: {
+                color: "#1976D2",
+                weight: 1.5,
+                fillColor: "#64B5F6",
+                fillOpacity: 0.35
+            }
+        }).addTo(map);
+    })
+    .catch(err => {
+        console.error("Lỗi:", err);
+    });
