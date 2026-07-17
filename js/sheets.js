3d84afc5911c3c761525ec5b48860076b4f03b48
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-17 11:06:53 +0700

    Update sheets.js

--- js/sheets.js
+++ js/sheets.js
@@ -1,1 +1,21 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxvF3U6jCOPVrV-9iSivSiX4ynVeusbjSF4nJmCrMuSok_dEaKSvGi-CRSXCPlrQ43d/exec";
 
+let gisData = {};
+
+async function loadGISData() {
+
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+    gisData = {};
+
+    data.forEach(item => {
+
+        gisData[item["Tên xã"]] = item;
+
+    });
+
+    console.log("Đã tải", Object.keys(gisData).length, "xã");
+
+}
