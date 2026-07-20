798da9435456e5438b46d444a653fe85f74b2fd4
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-20 09:22:30 +0700

    Update sheets.js

--- js/sheets.js
+++ js/sheets.js
@@ -1,21 +1,35 @@
+// ================= API GOOGLE SHEETS =================
+
 const API_URL = "https://script.google.com/macros/s/AKfycbxvF3U6jCOPVrV-9iSivSiX4ynVeusbjSF4nJmCrMuSok_dEaKSvGi-CRSXCPlrQ43d/exec";
 
 let gisData = {};
 
+// ================= ĐỌC DỮ LIỆU =================
+
 async function loadGISData() {
 
-    const response = await fetch(API_URL);
+    try {
 
-    const data = await response.json();
+        const response = await fetch(API_URL);
 
-    gisData = {};
+        const data = await response.json();
 
-    data.forEach(item => {
+        gisData = {};
 
-        gisData[item["ID"]] = item;
+        data.forEach(item => {
 
-    });
+            // Lưu theo ID
+            gisData[Number(item["ID"])] = item;
 
-    console.log("Đã tải", Object.keys(gisData).length, "xã");
+        });
+
+        console.log("Đã tải", Object.keys(gisData).length, "xã");
+        console.log(gisData);
+
+    } catch (err) {
+
+        console.error("Lỗi tải dữ liệu:", err);
+
+    }
 
 }
