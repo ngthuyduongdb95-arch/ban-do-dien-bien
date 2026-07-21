bad70c59cb3dc8327da25e7e4ec56c0bb7c613ef
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-21 16:53:16 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,70 @@
+//======================================================
+// APP.JS
+//======================================================
 
+document.addEventListener("DOMContentLoaded", async ()=>{
+
+    // Load dữ liệu Google Sheets
+    await loadSheet();
+
+    // Load bản đồ
+    await loadGeoJSON();
+
+    // Dashboard
+    dashboard.update();
+
+    //==========================
+    // Đổi lớp dữ liệu
+    //==========================
+
+    document
+        .getElementById("layerSelect")
+        .addEventListener("change",function(){
+
+            setLayer(this.value);
+
+        });
+
+    //==========================
+    // Tìm xã
+    //==========================
+
+    document
+        .getElementById("btnSearch")
+        .addEventListener("click",()=>{
+
+            searchFeature(
+
+                document
+                    .getElementById("txtSearch")
+                    .value
+
+            );
+
+        });
+
+    document
+        .getElementById("txtSearch")
+        .addEventListener("keypress",(e)=>{
+
+            if(e.key==="Enter"){
+
+                searchFeature(e.target.value);
+
+            }
+
+        });
+
+    //==========================
+    // Refresh
+    //==========================
+
+    document
+        .getElementById("btnRefresh")
+        .addEventListener("click",async()=>{
+
+            await reloadData();
+
+        });
+
+});
