d5a6bcb365ab45f8e214ccc10260d16f098c7aef
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-22 14:46:17 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -529,7 +529,7 @@ const labelOffset = {
     "Điện Biên Phủ": [0.010, -0.012],
     
     "Thanh Yên": [-0.006, 0.000],
-    "Thanh Nưa": [0, -0.008],
+    "Thanh Nưa": [0, -0.010],
     "Thanh An": [0.006, 0.000],
     "Mường Phăng": [0.000, -0.008],
     "Na Sang": [0.006, 0.006],
