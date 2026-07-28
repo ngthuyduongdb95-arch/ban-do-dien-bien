8ca1a143ced31c52c4a81f000a9b2cfb656cf9e6
ngthuyduongdb95-arch <ngthuyduongdb95@gmail.com>
2026-07-28 10:41:43 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -690,11 +690,10 @@ if(currentLayer==="PHUN"){
 div.innerHTML = `
     <h4>${cfg.title}</h4>
 
-    <div><i style="background:${cfg.color[0]}"></i>0</div>
     <div><i style="background:${cfg.color[1]}"></i>≤ ${b[1]}</div>
-<div><i style="background:${cfg.color[2]}"></i>${b[1]+1} – ${b[2]}</div>
-<div><i style="background:${cfg.color[3]}"></i>${b[2]+1} – ${b[3]}</div>
-<div><i style="background:${cfg.color[4]}"></i>> ${b[3]}</div>
+    <div><i style="background:${cfg.color[2]}"></i>${b[1]+1} – ${b[2]}</div>
+    <div><i style="background:${cfg.color[3]}"></i>${b[2]+1} – ${b[3]}</div>
+    <div><i style="background:${cfg.color[4]}"></i>> ${b[3]}</div>
 
     ${
     currentLayer === "DTLCP"
